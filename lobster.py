7aa729227ed72566ad59bb925d810d170b96178a
James Douglass <jdouglass@stanford.edu>
2019-07-10 13:17:00 -0700

    Major updates, initial conditions are now correct for the initial subregion.

--- lobster.py
+++ lobster.py
@@ -1,15 +1,13 @@
+import collections
 import pprint
 
 import numpy
 import pandas
 from natcap.invest import datastack
 
-# For each subregion
-#    For each timestep
-#        For each age/stage
 
-def get_subregion_names(dataframe):
-    return
+def beverton_holt(alpha, beta, spawners):
+    return (alpha*spawners) / (1 + (beta*spawners))
 
 
 def lobster():
@@ -19,15 +17,21 @@ def lobster():
 
     pprint.pprint(args)
 
-    alpha = numpy.float64(args['alpha'])
-    beta = numpy.float64(args['beta'])
-    if args['sexsp'].lower() == 'yes':
-        n_sexes = 2
-    else:
-        n_sexes = 1
-    n_init_recruits = numpy.int64(args['total_init_recruits'])
+    # lobster is age-based.
+    #alpha = numpy.float64(args['alpha'])
+    #beta = numpy.float64(args['beta'])
+    # n_init_recruits = numpy.int64(args['total_init_recruits'])
     n_timesteps = numpy.int64(args['total_timesteps'])
     unit_price = numpy.float64(args['unit_price'])
+
+    n_init_recruits = 4686959
+    alpha = numpy.float64(1000)
+    beta = numpy.float64(0.00000016069)
+
+    if args['sexsp'].lower() == 'yes':
+        n_sexes = 2.0
+    else:
+        n_sexes = 1.0
 
     # parsed population parameters
     population_params = pandas.read_csv(args['population_csv_path'])
@@ -40,7 +44,7 @@ def lobster():
         if subregion.startswith('Unnamed:'):
             break
 
-        per_subregion_params[subregion] = {'stages': {}}
+        per_subregion_params[subregion] = {'stages': collections.OrderedDict()}
 
         for row_index, row in population_params.iterrows():
             if all(row.isna()):
@@ -82,9 +86,70 @@ def lobster():
 
     total_spawners = [0]  # indexed by timestep
     total_recruits = [n_init_recruits]  # indexed by timestep
+    n_stages = len(per_subregion_params.values()[0]['stages'])
 
-    for timestep in range(1, n_timesteps + 1):
-        pass
+    # survival[subregion][stage_index]
+    survival = collections.defaultdict(list)
+    for subregion, subregion_params in per_subregion_params.items():
+        exploitation = subregion_params['ExploitationFraction']
+
+        for stage_index, stage in enumerate(subregion_params['stages'].values()):
+            survival[subregion].append(
+                stage['Mortality']*(1-(exploitation * stage['VulnFishing'])))
+
+    # populations[subregion][timestep][stage_index]
+    populations = collections.defaultdict(lambda: collections.defaultdict(list))
+    for timestep in range(0, n_timesteps + 1):
+        total_recruits = 0
+        total_population = 0
+
+        # Set initial conditions (different from normal modelling)
+        if timestep == 0:
+            for subregion, subregion_params in per_subregion_params.items():
+                larval_dispersal = subregion_params['LarvalDispersal']
+                for stage_index, stage in enumerate(subregion_params['stages'].values()):
+                    if stage_index == 0:
+                        population = (n_init_recruits/float(n_sexes)) * larval_dispersal
+                    elif stage_index < (n_stages -1):
+                        population = populations[subregion][0][stage_index-1] * survival[subregion][stage_index-1]
+                    else:
+                        population = (
+                            (populations[subregion][0][stage_index-1] * survival[subregion][stage_index-1]) /
+                            (1 - survival[subregion][stage_index]))
+
+                    populations[subregion][timestep].append(population)
+        else:
+            for subregion, subregion_params in per_subregion_params.items():
+                larval_dispersal = subregion_params['LarvalDispersal']
+                migration = 1  # No migration for now.
+
+                for stage_index, stage in enumerate(subregion_params['stages'].values()):
+                    if stage_index == 0:
+                        population = beverton_holt(alpha, beta, spawners=n_init_recruits/n_sexes)
+                    elif stage_index < (n_stages - 1):
+                        population = populations[subregion][timestep-1][stage_index-1] * survival[subregion][stage_index-1]
+                    else:  # We're at max stage
+                        population = (
+                            (populations[subregion][timestep-1][stage_index-1] * survival[subregion][stage_index-1]) +
+                            (populations[subregion][timestep-1][stage_index] * survival[subregion][stage_index]))
+
+                    # Can't have fewer than 0 individuals
+                    # Can't have fractional individuals.
+                    population = max(0, population)
+
+                    populations[subregion][timestep].append(population)
+
+    # produce 1 table per subregion
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
