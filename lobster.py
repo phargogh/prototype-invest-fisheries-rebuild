2f180f085ce808b513b342018e718518e5dc4997
James Douglass <jdouglass@stanford.edu>
2019-07-12 11:25:22 -0700

    Handling varied recriutment function parameters and optional migration in stage-based models.

--- lobster.py
+++ lobster.py
@@ -3,22 +3,38 @@ import os
 import glob
 import collections
 import pprint
+import logging
 
 import numpy
 import pandas
 from natcap.invest import datastack
 
 
-def beverton_holt_1(alpha, beta, spawners):
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+def beverton_holt_1(args):
+    alpha = args['alpha']
+    beta = args['beta']
+    spawners = args['spawners']
     return float(alpha*spawners) / (1. + (beta*spawners))
 
-
-def beverton_holt_2(alpha, beta, spawners):
+def beverton_holt_2(args):
+    alpha = args['alpha']
+    beta = args['beta']
+    spawners = args['spawners']
     return float(alpha * spawners) / (beta + spawners)
 
 
-def ricker(alpha, beta, spawners):
+def ricker(args):
+    alpha = args['alpha']
+    beta = args['beta']
+    spawners = args['spawners']
     return (alpha*spawners*math.e**(-beta*spawners))
+
+def fixed(args):
+    return args['n_recruits']
 
 
 def lobster():
@@ -47,8 +63,7 @@ def lobster():
     #beta = numpy.float64(2885000)
     #recruitment = beverton_holt_2
 
-
-    model(args)
+    model(args, recruitment=recruitment)
 
 def shrimp():
     # Uses fixed recruitment
@@ -58,10 +73,10 @@ def shrimp():
         '../../invest/data/invest-sample-data/white_shrimp_galveston_bay.invs.json')
     args = paramset.args.copy()
 
-    model(args)
+    model(args, recruitment=fixed)
 
 
-def model(args):
+def model(args, recruitment):
     n_timesteps = numpy.int64(args['total_timesteps'])
     n_init_recruits = numpy.int64(args['total_init_recruits'])
 
@@ -75,12 +90,28 @@ def model(args):
     else:
         model_type = 'age'
 
+    try:
+        alpha = args['alpha']
+    except KeyError:
+        alpha = None
+
+    try:
+        beta = args['beta']
+    except KeyError:
+        beta = None
+
+    try:
+        n_recruits = args['total_recur_recruits']
+    except KeyError:
+        n_recruits = None
+
+
     # parsed population parameters
     population_params = pandas.read_csv(args['population_csv_path'])
     per_subregion_params = collections.OrderedDict()
     per_stage_params = collections.OrderedDict()
     for subregion in population_params.columns:
-        if subregion.lower() == 'age_area':
+        if subregion.lower() in ('age_area', 'stage'):
             continue
 
         if subregion.startswith('Unnamed:'):
@@ -117,12 +148,13 @@ def model(args):
     for row_index, row in population_params.iterrows():
         if all(row.isna()):
             parameter_start_index = row_index
+            continue
         else:
             if row_index < parameter_start_index:
                 continue
 
         try:
-            # This the parameter name header
+            # This the parameter name header for lobster
             parameter_name = row['Age_Area']
         except KeyError:
             # shrimp uses this header instead.
@@ -140,18 +172,13 @@ def model(args):
 
             per_subregion_params[subregion_name][parameter_name] = row[subregion_name]
 
-    total_spawners = [0]  # indexed by timestep
-    total_recruits = [n_init_recruits]  # indexed by timestep
     n_stages = len(per_subregion_params.values()[0]['stages'])
     subregions = list(per_subregion_params.keys())
 
     # survival[subregion][stage_index]
     survival = collections.defaultdict(list)
     for subregion, subregion_params in per_subregion_params.items():
-        try:
-            exploitation = subregion_params['ExploitationFraction']
-        except KeyError:
-            import pdb; pdb.set_trace()
+        exploitation = subregion_params['ExploitationFraction']
 
         for stage_index, stage in enumerate(subregion_params['stages'].values()):
             survival[subregion].append(
@@ -159,32 +186,35 @@ def model(args):
 
     # migration[stage_name][from_subregion][to_subregion] = float
     migration = {}
-    for csv_filename in glob.glob(os.path.join(args['migration_dir'], '*.csv')):
-        stage_name = os.path.splitext(os.path.basename(csv_filename))[0].split('_')[-1]
-        temp_dict = pandas.DataFrame.to_dict(
-            pandas.read_csv(csv_filename, index_col=0), orient='dict')
+    if 'migration_dir' in args and args['migration_dir'] not in (None, ''):
+        for csv_filename in glob.glob(os.path.join(args['migration_dir'], '*.csv')):
+            stage_name = os.path.splitext(os.path.basename(csv_filename))[0].split('_')[-1]
+            temp_dict = pandas.DataFrame.to_dict(
+                pandas.read_csv(csv_filename, index_col=0), orient='dict')
 
-        # Pandas' to_dict() reads in the sink subregions as integers if they
-        # are integers, so we need to make sure that they are all strings so we
-        # can index the dict properly.
-        cast_keys_dict = {}
-        for source_subregion, sink_dict in temp_dict.items():
-            nested_dict = {}
-            for sink_subregion, migration_proportion in sink_dict.items():
-                nested_dict[str(sink_subregion)] = migration_proportion
+            # Pandas' to_dict() reads in the sink subregions as integers if they
+            # are integers, so we need to make sure that they are all strings so we
+            # can index the dict properly.
+            cast_keys_dict = {}
+            for source_subregion, sink_dict in temp_dict.items():
+                nested_dict = {}
+                for sink_subregion, migration_proportion in sink_dict.items():
+                    nested_dict[str(sink_subregion)] = migration_proportion
 
-            cast_keys_dict[source_subregion] = nested_dict
-        migration[stage_name] = cast_keys_dict
+                cast_keys_dict[source_subregion] = nested_dict
+            migration[stage_name] = cast_keys_dict
+    else:
+        LOGGER.info('No migration provided')
 
     # For keeping track of spawners and recruits
     # Indexed by timestep.
     total_spawners = [0]
     total_recruits = [n_init_recruits]
 
-    def population_after_migration(this_subregion, timestep, stage_index):
+    def population_after_migration(this_subregion, this_stage_name, timestep, stage_index):
         return sum(
             (populations[other_subregion][timestep][stage_index] *
-             migration[stage_name][other_subregion][this_subregion])
+             migration[this_stage_name][other_subregion][this_subregion])
             for other_subregion in subregions)
 
     # populations[subregion][timestep][stage_index]
@@ -193,11 +223,15 @@ def model(args):
         spawners = 0
 
         for subregion, subregion_params in per_subregion_params.items():
-            larval_dispersal_per_sex = subregion_params['LarvalDispersal'] / n_sexes
+            try:
+                larval_dispersal = subregion_params['LarvalDispersal']
+            except KeyError:
+                # UG specifies that larvae disperse equally across all
+                # subregions unless the user specifies a different proportion.
+                larval_dispersal = 1. / len(subregions)
+            larval_dispersal_per_sex = larval_dispersal / n_sexes
 
             for stage_index, (stage_name, stage) in enumerate(subregion_params['stages'].items()):
-                assert stage_index == int(stage_name)  # should be true for test data.
-
 
                 if model_type == 'age':
                     if stage_index == 0:
@@ -206,7 +240,7 @@ def model(args):
                     elif stage_index < (n_stages -1):
                         # disallow migration in the initial timestep.
                         if stage_name in migration and timestep > 0:
-                            population = population_after_migration(subregion, timestep-1, stage_index-1)
+                            population = population_after_migration(subregion, stage_name, timestep-1, stage_index-1)
 
                         else:
                             # Don't allow the population index to go below 0.
@@ -233,10 +267,10 @@ def model(args):
                         # and we're not at timestep 0.
                         if stage_name in migration and timestep > 0:
                             prev_stage_population = population_after_migration(
-                                subregion, timestep-1, stage_index-1) * survival_from_previous_stage
+                                subregion, stage_name, timestep-1, stage_index-1) * survival_from_previous_stage
 
                             final_stage_population = population_after_migration(
-                                subregion, timestep-1, stage_index)* survival_from_final_stage
+                                subregion, stage_name, timestep-1, stage_index)* survival_from_final_stage
 
                             population = prev_stage_population + final_stage_population
 
@@ -264,11 +298,17 @@ def model(args):
                                 (1-stage_survival**(stage['Duration']))))
 
                         if stage_index == 0:
-                            population = ((
-                                population_after_migration(
-                                    subregion, timestep-1, stage_index) *
-                                probability_of_staying_in_same_stage) + (
-                                    total_recruits[timestep]))
+                            if stage_name in migration:
+                                population = ((
+                                    population_after_migration(
+                                        subregion, stage_name, timestep-1, stage_index) *
+                                    probability_of_staying_in_same_stage) + (
+                                        total_recruits[timestep]))
+                            else:
+                                population = (
+                                    populations[subregion][timestep-1][stage_index] *
+                                    probability_of_staying_in_same_stage +
+                                    total_recruits[timestep])
                         else:
                             # G_asx in the User's Guide.  The probability of
                             # surviving from natural and fishing mortality and
@@ -277,13 +317,21 @@ def model(args):
                             probability_of_growing_to_next_stage = (
                                 (stage_survival**stage['Duration'] * (1-stage_survival)) /
                                 (1-stage_survival**stage['Duration']))
-                            population = (
-                                (population_after_migration(
-                                    subregion, timestep-1, stage_index-1) *
-                                 probability_of_growing_to_next_stage) +
-                                (population_after_migration(
-                                    subregion, timestep-1, stage_index) *
-                                 probability_of_staying_in_same_stage))
+
+                            if stage_name in migration:
+                                population = (
+                                    (population_after_migration(
+                                        subregion, stage_name, timestep-1, stage_index-1) *
+                                     probability_of_growing_to_next_stage) +
+                                    (population_after_migration(
+                                        subregion, stage_name, timestep-1, stage_index) *
+                                     probability_of_staying_in_same_stage))
+                            else:
+                                population = (
+                                    (populations[subregion][timestep-1][stage_index-1] *
+                                     probability_of_growing_to_next_stage) +
+                                    (populations[subregion][timestep-1][stage_index] *
+                                     probability_of_staying_in_same_stage))
                 else:
                     raise AssertionError('Invalid model type')
 
@@ -294,7 +342,13 @@ def model(args):
                 spawners += stage['Maturity'] * stage['Weight'] * population
 
         total_spawners.append(spawners)
-        total_recruits.append(max(0, recruitment(alpha, beta, spawners)))
+        recruitment_args = {
+            'alpha': alpha,
+            'beta': beta,
+            'spawners': spawners,
+            'n_recruits': n_recruits,
+        }
+        total_recruits.append(max(0, recruitment(recruitment_args)))
 
     # For now, just produce a dataframe for a single subregion for all timesteps.
     out_df = pandas.DataFrame.from_dict(populations['1'], orient='index')
